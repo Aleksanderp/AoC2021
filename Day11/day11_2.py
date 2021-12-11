771610992fef30051ceec9ba07cc69b4a0db1218
al-pi314 <aleksander.piciga@gmail.com>
2021-12-11 12:31:59 +0100

    day11

---
+++ Day11/day11_2.py
@@ -0,0 +1,42 @@
+octopuses = []
+flashes = 0
+with open("day11_2.txt", "r") as dat:
+    for line in dat:
+        octopuses.append(list(map(int, list(line.strip("\n\r")))))
+
+def flash(have_flashed, x, y):
+    if (x,y) in have_flashed:
+        return 0
+    if octopuses[y][x] <= 9:
+        return 0
+    have_flashed.add((x,y))
+    flashes = 0
+    for y_diff in range(-1, 2):
+        for x_diff in range(-1, 2):
+            if y_diff == 0 and x_diff == 0:
+                continue
+            if y + y_diff < 0 or y + y_diff >= len(octopuses):
+                continue
+            if x + x_diff < 0 or x + x_diff >= len(octopuses[0]):
+                continue
+            octopuses[y + y_diff][x + x_diff] += 1
+            flashes += flash(have_flashed, x + x_diff, y + y_diff)
+    return flashes + 1 
+
+step = 1
+while True:
+    have_flashed = set()
+    will_flash = []
+    for y in range(len(octopuses)):
+        for x in range(len(octopuses[0])):
+            octopuses[y][x] += 1
+            if octopuses[y][x] > 9:
+                will_flash.append((x, y))
+    for x, y in will_flash:
+        flashes += flash(have_flashed, x, y)
+    for x, y in have_flashed:
+        octopuses[y][x] = 0
+    if len(have_flashed) == (len(octopuses) * len(octopuses[0])):
+        break
+    step += 1
+print(step)
